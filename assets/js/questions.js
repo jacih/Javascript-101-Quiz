5a2683e9db6760d14d0e1e9cd9f04eeb18b77a69
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-23 09:18:10 -0400

    heavy debugging

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,31 +1,41 @@
 let qandAs = [{
-    quest: "What are the keywords used to create a new variable?",
-    ans: ["native, this, byte", "let, int, try", "const, var, let", "var, key, char"],
-    correct: "const, var, let",
+    quest: 'What are the keywords used to create a new variable?',
+    ans: ['native, this, byte', 'let, int, try', 'const, var, let', 'var, key, char'],
+    correct: 'const, var, let',
   },
   {
-    quest: "What is the first index of a javascript object?",
-    ans: ["indexOf(1)", "1", "0", "-1"],
-    correct: "0", 
+    quest: 'What is the first index of a javascript object?',
+    ans: ['indexOf(1)', '1', '0', 'indexOf[1]', '-1'],
+    correct: '0',
    },
    {
-    quest: "What data type are undefined, null, symbol, and bigint examples of?",
-    ans: ["Non-Primative", "Operands", "Authentic", "Primitive"],
-    correct: "Primitive", 
+    quest: 'What data type are undefined, null, symbol, and bigint examples of?',
+    ans: ['Non-Primative', 'Operands', 'Authentic', 'Primitive'],
+    correct: 'Primitive',
    },
    {
-    quest: "What type of function are sort, reduce, filter, and map examples of?",
-    ans: ["Innate", "Higher-order", "Complex", "Master"],
-    correct: "Higher-order", 
+     quest: 'Objects, arrays, and classes are the same data containers',
+     ans: ['True', 'False'],
+     correct: 'False',
    },
    {
-    quest: "What method can you use to check if an object is an array?",
-    ans: ["Array.isArray()", "typeOf()", "Object.isObject()", "setPrototypeOf()"],
-    correct: "Array.isArray()", 
+    quest: 'What type of function are sort, reduce, filter, and map examples of?',
+    ans: ['Innate', 'Higher-order', 'Complex', 'Master', 'Universal'],
+    correct: 'Higher-order', 
    },
    {
-    quest: "Which of these methods do NOT create a loop?",
-    ans: ["for", "switch", "while", "do...while"],
-    correct: "switch", 
+    quest: 'What method can you use to check if an object is an array?',
+    ans: ['Array.isArray()', 'typeOf()', 'Object.isObject()', 'setPrototypeOf()'],
+    correct: 'Array.isArray()', 
+   },
+   {
+    quest: 'Which of these methods do NOT create a loop?',
+    ans: ['for', 'do...while', 'for...in', 'switch', 'while'],
+    correct: 'switch', 
+   },
+   {
+     quest: 'What are the different variable scopes in Javascript?',
+     ans: ['Local, Universal, Relational', 'Global, Local', 'Parent, Child, Sibling', 'Functional, Universal'],
+     correct: 'Global, Local'
    }
   ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,72 @@
 //declaring elements that exist in html;
-let timeLabelEl = document.querySelector("#timer-label");
-let timerEl = document.querySelector("#timer");
-let contentEl = document.querySelector(".content");
-let promptEl = document.querySelector("#quiz-prompt");
-let startBtnEl = document.querySelector("#start");
+const scoresBtnEl = document.querySelector('#scoresBtn'); 
+const timeLabelEl = document.querySelector('#timer-label');
+const timerEl = document.querySelector('#timer');
+const contentEl = document.querySelector('.content');
+const promptEl = document.querySelector('#quiz-prompt');
+const startBtnEl = document.querySelector('#start');
+const quizResEl = document.querySelector('#quiz-results');
+const quizFormEl = document.querySelector('#quiz-form');
+const scoreEl = document.querySelector('#score');
+const userInputEl = document.querySelector('#initials');
+const retryEl = document.querySelector('#retryBtn');
+const submitEl = document.querySelector('#submitBtn');
+const hiscoresEl = document.querySelector('#scores-card');
+const highListEl = document.querySelector('#highscores-list');
+const clearEl = document.querySelector('#clear-button');
+const resetEl = document.querySelector('#resetBtn');
 
 // declare elements that need to be created then appended to html;
-let quizContentEl = document.createElement("article");
-quizContentEl.setAttribute("class", "quiz-content");
-let questionEl = document.createElement("h2");
-questionEl.setAttribute("class", "questions");
-let ansGroupEl = document.createElement("div");
-ansGroupEl.setAttribute("class", "answer-group");
-let ansPrompt = document.createElement("p");
-ansPrompt.setAttribute("class", "answer-prompt");
+const quizContentEl = document.createElement('article');
+quizContentEl.setAttribute('class', 'quiz-content');
+const questionEl = document.createElement('h2');
+questionEl.setAttribute('class', 'questions');
+const ansGroupEl = document.createElement('div');
+ansGroupEl.setAttribute('class', 'answer-group');
+const ansPrompt = document.createElement('p');
+ansPrompt.setAttribute('class', 'answer-prompt');
 
 //declaring global variables;
 let score = 0;
 let timeLeft = 60;
 let index = 0;
+let stored = [];
 
 // countdown function; to be called on start click
-function timerCountdown() {
+const timerCountdown = () => { 
   let timerInterval = setInterval(function() {
+    if (timeLeft > 0) {
     timeLeft--;
-    timerEl.textContent = timeLeft + " seconds remaining";
-
-    if (timeLeft <= 0) {
+    timerEl.textContent = timeLeft + ' seconds remaining';
+    } else {
       clearInterval(timerInterval);
-      quizResults();
+      hide(timerEl);
+      saveResults();
     }
   }, 1000);
 }
 
 //function to render questionEl and related answer buttons;
-function renderQuiz() {
+const renderQuiz = () => { 
   hide(promptEl);
-  questionEl.textContent = qandAs[index].quest;
-  // console.log(qandAs[index].quest);
-  let answers = qandAs[index].ans;
-  // console.log(answers);
+  show(quizContentEl);
+  document.querySelectorAll('.answer').forEach(item => item.remove());
   
-  //how I am making the buttons is causing duplication of last answer of previous question on index++ loop >:(
-  if (index === 0) {
+  if (index < qandAs.length) {
+    questionEl.textContent = qandAs[index].quest;
+  
+    let answers = qandAs[index].ans;
+    // console.log(answers);
     for (let a = 0; a < answers.length; a++) {
-      let ansBtnEl = document.createElement("button");
-      ansBtnEl.setAttribute("class", "answer");
+      let ansBtnEl = document.createElement('button');
+      ansBtnEl.setAttribute('class', 'answer');
       ansBtnEl.textContent = answers[a];
-      // console.log(ansBtnEl);
+      ansBtnEl.addEventListener('click', isCorrect);
       ansGroupEl.appendChild(ansBtnEl);
-      //add event listener to each button and calls isCorrect function to check answer;
-      ansBtnEl.addEventListener("click", isCorrect);
     }
-  } else {
-    for (let a = 0; a < answers.length; a++) {
-      let ansBtnEl = document.querySelector("button");
-      ansBtnEl.innerText = answers[a];
-      //add event listener to each button and calls isCorrect function to check answer;
-      ansBtnEl.addEventListener("click", isCorrect);
-      }
+  }
+  if (index > qandAs.length - 1) {
+    saveResults();
   }
   quizContentEl.append(questionEl);
   quizContentEl.append(ansGroupEl);
@@ -66,27 +74,17 @@ function renderQuiz() {
   quizContentEl.appendChild(ansPrompt);
 }
 
-function hide(element) {
-  element.setAttribute("style", "display: none;");
-}
-
-function show(element) {
-  element.setAttribute("style", "display: block;");
-}
-
-function isCorrect(event) {
-  let choice = event.target.textContent;
-  // console.log(choice);
-  let rightAns = qandAs[index].correct;
-  // console.log(rightAns);
+const isCorrect = (event) => {
+  const choice = event.target.textContent;
+  const rightAns = qandAs[index].correct;
   let isCorrect = true;
 
   if (choice !== rightAns) {
     !(isCorrect);
-    ansPrompt.textContent = "Incorrect!";
+    ansPrompt.textContent = 'Incorrect!';
       if (timeLeft <= 10) {
         timeLeft = 0;
-        quizResults();
+        saveResults();
       } else {
         timeLeft -= 10;
       }
@@ -94,25 +92,117 @@ function isCorrect(event) {
   } else {
     (isCorrect);
     score += 10;
-    ansPrompt.textContent = "Correct!";
+    ansPrompt.textContent = 'Correct!';
     nextQuestion();
   }
 
-  let removePrompt = setTimeout(function() {
+  const removePrompt = setTimeout(function() {
     ansPrompt.remove();
   }, 1000);
 }
 
-function nextQuestion() {
-  if (index < qandAs.length - 1) {
+const nextQuestion = () => {  
+  if (index <= qandAs.length - 1) {
     renderQuiz(index++);
-  } else {
-    hide(quizContentEl);
-    quizResults();
+  }
+  if (index > 8) {
+  saveResults();
   }
 }
 
-startBtnEl.addEventListener("click", function() {
+const saveResults = () => {
+  hide(timerEl);
+  hide(quizContentEl);
+  hide(hiscoresEl);
+  show(quizResEl);
+
+  scoreEl.innerHTML = score + ' out of ' + (qandAs.length * 10) + '.';
+  
+  submitEl.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let initials = userInputEl.value;
+    let user = {
+      name: initials,
+      score: score
+    };
+
+    if (initials === '') {
+      window.alert('Please enter your initials to save');
+      return;
+    } else {
+      stored.push(user);
+      initials = '';
+      localStorage.setItem('High Scores', JSON.stringify(user))
+      showHighScores();
+    }
+  });
+}
+
+const showHighScores = () => {
+  hide(quizResEl);
+  hide(promptEl);
+  show(hiscoresEl);
+  stored = JSON.parse(localStorage.getItem('High Scores'));
+
+  if (stored !== null) {
+    stored.forEach(user => {
+      let scorerEl = document.createElement('ol');
+      scorerEl.setAttribute('class', 'saved-scorer');
+      scorerEl.textContent = user.name + '-' + user.score;
+      highListEl.appendChild(scorerEl);
+    });
+  }
+}
+
+// assisting functions to hide or show html elements
+const hide = (e) => {
+  e.setAttribute('style', 'display: none;');
+}
+
+const show = (e) => {
+  e.setAttribute('style', 'display: block;');
+}
+
+// navigational button event listeners
+scoresBtnEl.addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  showHighScores();
+});
+
+startBtnEl.addEventListener('click', function(e) {
+  e.preventDefault();
   timerCountdown();
   renderQuiz();
 });
+  
+submitEl.addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+clearEl.addEventListener('click', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  localStorage.removeItem('High Scores');
+  stored = [];
+  location.reload();
+});
+
+resetEl.addEventListener('click', function (e) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  location.reload();  
+});
+
+// function to start quiz;
+const init = () => {
+  hide(quizContentEl);
+  hide(quizResEl);
+  hide(hiscoresEl);
+  show(promptEl);
+}
+
+init();
